9a58036c6775c067307927ac92b99ff9d66510dd
hoverboards <41450930+meliketanrikulu@users.noreply.github.com>
2021-06-05 17:51:38 +0300

    Delete roc_curve.m

--- roc_curve.m
+++
@@ -1,7 +0,0 @@
-
-[X,Y,T,AUC] = perfcurve(species(51:end,:),scores,'virginica');
-AUC
-plot(X,Y)
-xlabel('False positive rate') 
-ylabel('True positive rate')
-title('ROC for Classification by Logistic Regression')
